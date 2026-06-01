aa64a044c72c3e15dc44741ce049698104da7ae1
praneshkA <pranesh.ka2023it@sece.ac.in>
2026-06-01 23:08:32 +0530

    terraform: configure remote state backend

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,4 +1,12 @@
 terraform {
+  backend "s3" {
+    bucket         = "devops-2tier-tfstate-praneshka"
+    key            = "devops-2tier-cicd/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "devops-terraform-locks"
+    encrypt        = true
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
